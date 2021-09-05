0537e517080a2ed2f6fb6c70676f79e677f677b0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-09-05 18:15:55 +0530

    RM6785: power-libperfmgr: Properly handle sustained performance mode
     * when tested with [1], even after exiting the app, spm was
       still active in powerhal thereby keeping max freqs limited
       and blocking boosts
    
     * enable/disable it properly and end all other hints before
       enabling, to fix this issue
    
     * while we're at it, don't handle max freq for anything other
       than spm in powerhint
    
    [1] https://android.googlesource.com/platform/cts/+/refs/heads/master/hostsidetests/sustainedperf
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I8962b86a3bb2509021daf7ed00507bccc4f532e0

--- aidl/power-libperfmgr/Power.cpp
+++ aidl/power-libperfmgr/Power.cpp
@@ -80,9 +80,12 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     switch (type) {
         case Mode::SUSTAINED_PERFORMANCE:
             if (enabled) {
+                endAllHints();
                 HintManager::GetInstance()->DoHint("SUSTAINED_PERFORMANCE");
+            } else {
+                HintManager::GetInstance()->EndHint("SUSTAINED_PERFORMANCE");
             }
-            mSustainedPerfModeOn = true;
+            mSustainedPerfModeOn = enabled;
             break;
         case Mode::LOW_POWER:
             if (enabled) {
@@ -94,9 +97,6 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
             mBatterySaverOn = enabled;
             break;
         case Mode::LAUNCH:
-            if (mSustainedPerfModeOn) {
-                break;
-            }
             [[fallthrough]];
         case Mode::DOUBLE_TAP_TO_WAKE:
             [[fallthrough]];
@@ -113,7 +113,9 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
         case Mode::AUDIO_STREAMING_LOW_LATENCY:
             [[fallthrough]];
         default:
-            if (mBatterySaverOn) break;
+            if (mBatterySaverOn || mSustainedPerfModeOn) {
+                break;
+            }
             if (enabled) {
                 HintManager::GetInstance()->DoHint(toString(type));
             } else {
