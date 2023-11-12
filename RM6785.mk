0641e851445a8cc273fc3d60a7ed5573aa44b9e8
Woomymy <woomy@woomy.be>
2023-11-12 13:20:42 +0100

    RM6785: Link sensor blobs against `android.hardware.sensors@1.0-convert`
    * `libndksensorbridge` was converted to AIDL and is no longer linked to
      this static library that contains symbols required by our sensor
      blobs. Add a shim linked to `android.hardware.sensors@1.0-convert` to
      fix our sensors
    
    Change-Id: I969f97bb15d0abcc71f87352722995db3826e122
    Signed-off-by: Woomymy <woomy@woomy.be>

--- RM6785.mk
+++ RM6785.mk
@@ -285,7 +285,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0.vendor:64 \
     android.frameworks.sensorservice@1.0.vendor \
-    libsensorndkbridge
+    libsensorndkbridge \
+    libshim_sensors
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
