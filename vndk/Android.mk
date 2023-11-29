5557bdd1b26e374cef50b9d49d1bfed5bdd0ce8e
SamarV-121 <samar@samarv121.dev>
2023-11-29 11:18:48 +0530

    RM6785: vndk: Move libui-v32 definition back to Android.mk
    Change-Id: Ia7252ed7bdf85dc6eb2c9e89c70e0d6655e8315a

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,3 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libui-v32
+LOCAL_SRC_FILES := libui-v32.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TARGET_ARCH := arm
+LOCAL_MODULE_TAGS := optional
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_VENDOR_MODULE := true
+LOCAL_REQUIRED_MODULES := android.hardware.graphics.common-V2-ndk_platform
+include $(BUILD_PREBUILT)
+
 ifeq ($(LINEAGE_BUILD),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.graphics.common-V2-ndk_platform
