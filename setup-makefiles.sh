7325d1177fa95a5f434b4c96dfc755439f562192
Bruno Martins <bgcngm@gmail.com>
2024-01-16 22:09:40 +0000

    RM6785: Add firmware extraction support
    Change-Id: I61b0f4cdddf65166cb12a39926e1fcb504241975

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -29,6 +29,9 @@ write_headers "RMX2001L1 RMX2151L1"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Append the calls to firmware images
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
 
 # Finish
 write_footers
