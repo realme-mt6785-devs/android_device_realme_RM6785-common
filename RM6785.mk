571b5c89bc7f9419f29a21365a883010bd04640b
Kelvin Zhang <zhangkelvin@google.com>
2024-06-25 16:57:21 -0700

    RM6785: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd

--- RM6785.mk
+++ RM6785.mk
@@ -322,6 +322,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # Update
+AB_OTA_UPDATER := false
 PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
 
 # USB
