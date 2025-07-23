elohim-etz/android_device_RM6785

RM6785: Shut hwcomposer spam
Change-Id: I9162e1f85e282fe66727f76abbfadad53e8c0df0
Signed-off-by: Naveen <imnaveenbisht@gmail.com>

vendor_prop.mk
@@ -31,6 +31,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.GsmCallTkrHlpr=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.GsmCdmaConn=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.GsmCdmaPhone=$(VENDOR_LOG_LEVEL_I) \
+    persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.IccCardProxy=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.IccPhoneBookIM=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.IccProvider=$(VENDOR_LOG_LEVEL_I) \

realme-mt6785-devs/android_device_realme_RM6785-common

vendor_prop.mk
@@ -31,6 +31,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.GsmCallTkrHlpr=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.GsmCdmaConn=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.GsmCdmaPhone=$(VENDOR_LOG_LEVEL_I) \
+    persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.IccCardProxy=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.IccPhoneBookIM=$(VENDOR_LOG_LEVEL_I) \
     persist.log.tag.IccProvider=$(VENDOR_LOG_LEVEL_I) \